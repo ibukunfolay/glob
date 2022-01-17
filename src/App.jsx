f1af3740cf362084d586e3972fa4c87fffaba8cd
ibukunfolay <70657237+ibukunfolay@users.noreply.github.com>
2022-01-17 07:09:30 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   if (isLoading) {
     return (
-      <div className="flex min-h-screen w-full justify-center item-center">
+      <div className="flex min-h-auto pt-[15em] w-auto justify-center align-center">
         <img src={gif}></img>
       </div>
     );
