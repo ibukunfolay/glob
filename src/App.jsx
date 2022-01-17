2bd5df4156f0e4aab17ba37be7b2a8dfac196fc6
ibukunfolay <70657237+ibukunfolay@users.noreply.github.com>
2022-01-17 06:32:23 +0100

    update dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Client from "./Pages/Client";
 import Landing from "./Pages/Landing";
 import Signup from "./Pages/Sign";
 import Signin from "./Pages/Signin";
@@ -11,6 +12,7 @@ const App = () => {
         <Route path={`${"/" || "/home"}`} element={<Landing />} />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Signin />} />
+        <Route path="dashboard" element={<Client />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/Client.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../Components/Dashboard/Dashboard";
+import Sidebar from "../Components/Dashboard/Sidebar";
+
+const Client = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Client;
